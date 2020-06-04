ba616eef6ef69ac9d17883028b20914424c6f391
Tommy Gomez <tjk2132@columbia.edu>
2020-06-04 00:20:24 +0000

    implement working deletion of comments

--- portfolio/src/main/java/com/google/sps/servlets/DeleteDataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DeleteDataServlet.java
@@ -5,11 +5,7 @@ import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
-import com.google.appengine.api.datastore.Query.SortDirection;
-import com.google.appengine.api.datastore.FetchOptions;
 import java.io.IOException;
-import java.util.*;
-import com.google.gson.Gson;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -26,15 +22,9 @@ public class DeleteDataServlet extends HttpServlet {
   }
 
   @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    
-  }
-
-  @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    PreparedQuery comments = datastore.prepare(new Query("Comments")); 
+    PreparedQuery comments = datastore.prepare(new Query("Comment")); 
     for (Entity entity : comments.asIterable()) {
-      System.out.println("iterating through the comments");
       datastore.delete(entity.getKey());
     }
     response.sendRedirect("/index.html");
