6003abcc9f917cd809cde3e6c9ab3207679e9819
Tommy Gomez <tjk2132@columbia.edu>
2020-06-04 17:36:26 +0000

    reformat comments to follow style guidelines

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -44,23 +44,23 @@ function displayTab(event, tabName) {
  * Displays the timeline animation on the Interests tab.
  */
 function drawTimeline() {
-  //branches and the images to their starting positions
+  // Resets branches and images to their starting positions.
   hideImages();
   hideBranches();
 
   var timeline = document.getElementById("timeline");
   var movingEndpoint = document.getElementById("moving-endpoint")
 
-  //moves the right end of the timeline, resulting in the left-to-right animation
+  // Moves the right end of the timeline, resulting in the left-to-right animation.
   var timelineRightPosition = 95;
 
-  //moves the "Present" endpoint to the right as the timeline expands
+  // Moves the "Present" endpoint to the right as the timeline expands.
   var endpointLeftPosition = 97 - timelineRightPosition;
 
-  //controls the direction of each branch's animation (true expands up, false expands down)
+  // Controls the direction of each branch's animation (true expands up, false expands down).
   var branchUp = true;
 
-  //triggers each branch's animation at the appropriate point in the timeline's expansion
+  // Triggers each branch's animation at the appropriate point in the timeline's expansion.
   var branchTriggers = {
     6: "astronomy",
     22: "geography",
@@ -72,25 +72,25 @@ function drawTimeline() {
 
   var timelineAnimation = setInterval(extendTimeline, 20);
   function extendTimeline() {
-    //stops the timeline's animation
+    // Stops the timeline's animation.
     if (timelineRightPosition <= 5) {
       clearInterval(timelineAnimation);
     } else {
-      //increment the positions of the timeline and endpoint, and set respective their HTML style attributes
+      // Increments the positions of the timeline and endpoint, and sets their respective HTML style attributes.
       timelineRightPosition -= 0.5;
       endpointLeftPosition = 97 - timelineRightPosition;
       timeline.style.right = timelineRightPosition + "%";
       movingEndpoint.style.left = endpointLeftPosition + "%";
 
-      //animate the branches once the timeline reaches their trigger points
+      // Animates the branches once the timeline reaches their trigger points.
       if (endpointLeftPosition in branchTriggers) {
-        //display the branch's image
+        // Displays the branch's image.
         displayImage(branchTriggers[endpointLeftPosition]);
 
-        //start the branch's animation
+        // Starts the branch's animation.
         displayTimelineBranch(branchTriggers[endpointLeftPosition], branchUp);
 
-        //flip the animation direction for the next branch, resulting in branches that alternate between up and down
+        // Flips the animation direction for the next branch, resulting in branches that alternate between up and down.
         branchUp = !branchUp;
       }
     }
@@ -122,22 +122,22 @@ function displayImage(imageID) {
 function displayTimelineBranch(branchName, branchUp) {
   var branch = document.getElementById(branchName);
 
-  //depending on the value of branchUp, this changes either the "top" or "bottom" attribute of the branch
+  // Depending on the value of branchUp, this changes either the "top" or "bottom" attribute of the branch.
   var positionToExtend = 50;
 
   var branchAnimation = setInterval(extendTimelineBranch, 20);
   function extendTimelineBranch() {
-    //stop the branch's animation
+    // Stop the branch's animation.
     if (positionToExtend <= 5) {
       clearInterval(branchAnimation);
     } else {
-      //increment the position of the branch, resulting in the animation
+      // Increment the position of the branch, resulting in the animation.
       positionToExtend -= 0.5;
 
-      //animate up by changing the branch's "top" attribute
+      // Animate up by changing the branch's "top" attribute.
       if (branchUp === true) {
         branch.style.top = positionToExtend + "%";
-      //animate down by changing the branch's "bottom" attribute
+      // Animate down by changing the branch's "bottom" attribute.
       } else {
         branch.style.bottom = positionToExtend + "%";
       }
